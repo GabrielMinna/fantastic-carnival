4207788d45d304438bce35d7eb8b6199d45a5ba3
Gabriel Ramirez <wagara83@gmail.com>
2022-03-09 02:39:04 -0300

    Add hijitus and pichichus

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Application, Sprite } from 'pixi.js'
+import { Application, Loader, Sprite } from 'pixi.js'
 
 const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
@@ -9,11 +9,19 @@ const app = new Application({
 	height: 480
 });
 
-const clampy: Sprite = Sprite.from("clampy.png");
+Loader.shared.add({url:"./Hijitus_pichichus.png", name:"Hijitus_pichichus"});
+Loader.shared.add({url:"./clampy.png", name:"CLAMPY"});
+
+Loader.shared.onComplete.add(()=>{
+const clampy: Sprite = Sprite.from("./Hijitus_pichichus.png");
+
+console.log("hola mundo", clampy.width , clampy.height);
 
 clampy.anchor.set(0.5);
 
 clampy.x = app.screen.width / 2;
-clampy.y = app.screen.height / 2;
+clampy.y = app.screen.height / 2; 
 
 app.stage.addChild(clampy);
+});
+Loader.shared.load(); 
